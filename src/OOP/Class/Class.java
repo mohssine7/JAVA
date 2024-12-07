e91d3cb394ff1d1dd7222a66fffe14851cd17868
mohssine7 <mohssinezernoun2@gmail.com>
2024-12-07 02:19:55 +0100

    ajout de construct get and set pour définir la classse

--- src/OOP/Class/Class.java
+++ src/OOP/Class/Class.java
@@ -4,6 +4,18 @@ public class Class {
     // We need to add the attributes of the class
     String description;
 
+    public Class(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     // then the functions of the class
     void afficherDescription()
     {
